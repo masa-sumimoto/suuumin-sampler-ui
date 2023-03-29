8b9f007309cecaa2e729dcf55a4b459d9a30e7d0
Masaaki Sumimoto <masa.sumimoto@gmail.com>
2023-03-29 18:01:37 +0900

    Add minimum styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="meta-wrap">
+      <h1 className="meta-ttl">SUUUMIN SAMPLER UI</h1>
+      <div className="app-wrap">
+        <div className="app">this is app!</div>
+      </div>
     </div>
   );
 }
