9683a0bf6fb3a9e7e923cc4aeabcf25abb6598ec
Masaaki Sumimoto <masa.sumimoto@gmail.com>
2023-04-02 23:03:13 +0900

    Layout optimization for swipe pad

--- src/components/Utils.tsx
+++ src/components/Utils.tsx
@@ -7,7 +7,7 @@ const Utils = () => {
   return (
     <div className="app-col-12">
       <div className="app-row app-u-justify-content-between">
-        <div className="app-col-auto">
+        <div className="app-col-6">
           <div className="app-u-d-flex app-u-d-gap-gutter">
             <div className="bank-btn">A</div>
             <div className="bank-btn">B</div>
@@ -15,8 +15,10 @@ const Utils = () => {
             <div className="bank-btn">D</div>
           </div>
         </div>
-        <div className="app-col-auto">
-          <div className="swipe-pad"></div>
+        <div className="app-col-6">
+          <div className="app-u-justify-content-end app-u-d-flex">
+            <div className="swipe-pad"></div>
+          </div>
         </div>
       </div>
     </div>
