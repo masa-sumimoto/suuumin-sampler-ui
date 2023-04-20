08857bd3443f823d7fdf8eb503fb37ee5acfae7a
Masaaki Sumimoto <masa.sumimoto@gmail.com>
2023-04-20 19:38:32 +0900

    Update BPM Panel Function and Design

--- src/components/BPMPanel.tsx
+++ src/components/BPMPanel.tsx
@@ -1,43 +1,92 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useRef, useEffect } from "react";
+import { useTimeStore } from "../stores/timeStore";
+
+import iconUp from "../images/icon-up.svg";
+import iconDown from "../images/icon-down.svg";
+import iconDoubleUp from "../images/icon-double-up.svg";
+import iconDoubleDown from "../images/icon-double-down.svg";
 
 const BPMPanel = () => {
-  const [num, setNum] = useState<number>(120);
   const panelRef = useRef<HTMLDivElement | null>(null);
+  const {
+    bpm,
+    changeBpm,
+    incrementBpm,
+    decrementBpm,
+    incrementMoreBpm,
+    decrementMoreBpm,
+  } = useTimeStore();
 
   useEffect(() => {
     const panel = panelRef.current;
-
     if (panel) {
       const handleScroll = (event: WheelEvent) => {
-        const newNum = num + (event.deltaY > 0 ? 1 : -1);
+        console.log("handle");
+        const newNum = bpm + (event.deltaY > 0 ? 1 : -1);
+        changeBpm(newNum);
         if (newNum < 40) {
-          setNum(40);
+          changeBpm(40);
         } else if (newNum > 255) {
-          setNum(255);
+          changeBpm(255);
         } else {
-          setNum(newNum);
+          changeBpm(newNum);
         }
       };
-
       panel.addEventListener("wheel", handleScroll);
 
       return () => {
         panel.removeEventListener("wheel", handleScroll);
       };
     }
-  }, [num]);
+  }, [bpm]);
+
+  const handleBpmUpBtnClick = () => {
+    incrementBpm();
+  };
+
+  const handleBpmDownBtnClick = () => {
+    decrementBpm();
+  };
+
+  const handleBpmUp10BtnClick = () => {
+    incrementMoreBpm();
+  };
+
+  const handleBpmDown10BtnClick = () => {
+    decrementMoreBpm();
+  };
 
   return (
     <div className="bpm-panel">
       <div ref={panelRef} className="bpm-panel__display">
-        <span className="bpm-panel__label">BPM : </span>
-        <span className="bpm-panel__num">{num}</span>
+        <span className="bpm-panel__label">BPM :</span>
+        <span className="bpm-panel__num">{bpm}</span>
       </div>
       <div className="bpm-panel__btns">
-        <button className="bpm-panel__btn --type_up">U</button>
-        <button className="bpm-panel__btn --type_down">D</button>
-        <button className="bpm-panel__btn --type_up">U</button>
-        <button className="bpm-panel__btn --type_down">D</button>
+        <button
+          className="bpm-panel__btn --type_up"
+          onClick={handleBpmUpBtnClick}
+        >
+          <img src={iconUp} alt="Up" />
+        </button>
+        <button
+          className="bpm-panel__btn --type_down"
+          onClick={handleBpmDownBtnClick}
+        >
+          <img src={iconDown} alt="Down" />
+        </button>
+        <button
+          className="bpm-panel__btn --type_up"
+          onClick={handleBpmUp10BtnClick}
+        >
+          <img src={iconDoubleUp} alt="10 Up" />
+        </button>
+        <button
+          className="bpm-panel__btn --type_down"
+          onClick={handleBpmDown10BtnClick}
+        >
+          <img src={iconDoubleDown} alt="10 Down" />
+        </button>
       </div>
     </div>
   );
--- src/stores/timeStore.ts
+++ src/stores/timeStore.ts
@@ -2,13 +2,53 @@ import { create } from "zustand";
 
 type State = {
   measure: number;
+  bpm: number;
+  minBpm: number;
+  maxBpm: number;
+  bpmUpSIze: number;
+  bpmMoreUpSIze: number;
 };
 
 type Action = {
   changeMeasure: (v: number) => void;
+  changeBpm: (v: number) => void;
+  incrementBpm: () => void;
+  decrementBpm: () => void;
+  incrementMoreBpm: () => void;
+  decrementMoreBpm: () => void;
 };
 
 export const useTimeStore = create<State & Action>()((set, get) => ({
   measure: 1,
+  bpm: 120,
+  minBpm: 40,
+  maxBpm: 255,
+  bpmUpSIze: 1,
+  bpmMoreUpSIze: 10,
   changeMeasure: (v) => set({ measure: v }),
+  changeBpm: (v) => set({ bpm: v }),
+  incrementBpm: () => {
+    const { bpm, maxBpm, bpmUpSIze } = get();
+    let newBpm = bpm + bpmUpSIze;
+    newBpm = newBpm <= maxBpm ? newBpm : maxBpm;
+    set({ bpm: newBpm });
+  },
+  decrementBpm: () => {
+    const { bpm, minBpm, bpmUpSIze } = get();
+    let newBpm = bpm - bpmUpSIze;
+    newBpm = newBpm >= minBpm ? newBpm : minBpm;
+    set({ bpm: newBpm });
+  },
+  incrementMoreBpm: () => {
+    const { bpm, maxBpm, bpmMoreUpSIze } = get();
+    let newBpm = bpm + bpmMoreUpSIze;
+    newBpm = newBpm <= maxBpm ? newBpm : maxBpm;
+    set({ bpm: newBpm });
+  },
+  decrementMoreBpm: () => {
+    const { bpm, minBpm, bpmMoreUpSIze } = get();
+    let newBpm = bpm - bpmMoreUpSIze;
+    newBpm = newBpm >= minBpm ? newBpm : minBpm;
+    set({ bpm: newBpm });
+  },
 }));
